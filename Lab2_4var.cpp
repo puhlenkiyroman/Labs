c9136864a0fa857841652597d0a062b825fe0924
Roman Litvinenko <80386499+puhlenkiyroman@users.noreply.github.com>
2021-03-10 15:11:08 +0600

    Add files via upload

---
+++ Lab2_4var.cpp
@@ -0,0 +1,27 @@
+﻿// Lab2_4var.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+#include <cmath>
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double x, N;
+    printf("Ввведите значение |x| < 1 : ");
+    int count_of_x = scanf_s("%lf", &x);
+    if (count_of_x != 1 || x >= 1 ) 
+    {
+        printf("Вы ввели не число или оно больше, либо равно единице");
+        return 0;
+    }
+    else {}
+    N = 1 /pow((1 + abs(x)),2);
+    cout << "Значение вычисленного ряда равняется " << N;
+    return 0;
+}
+
