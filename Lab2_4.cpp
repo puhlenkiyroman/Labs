bf92b8e62e35652b6fc5181a83883485207aee3b
Roman Litvinenko <80386499+puhlenkiyroman@users.noreply.github.com>
2021-03-22 18:38:12 +0600

    Update Lab2_4.cpp

--- Lab2_4.cpp
+++ Lab2_4.cpp
@@ -12,20 +12,20 @@ int main()
     setlocale(LC_ALL, "Russian");
 
     double x, sum = 0.0;
-    const int n = 10;
+    const int n = 100;
     printf("Ввведите значение |x| < 1 : ");
     int count_of_x = scanf_s("%lf", &x);
-    if (count_of_x != 1 || x >= 1 ) 
+    if (count_of_x != 1 || x >= 1)
     {
         printf("Вы ввели не число или оно больше, либо равно единице");
         return 0;
     }
     else {}
-    for (int i = 0; i <= n; i++) 
+    x = fabs(x);
+    for (int i = 0; i <= n; i++)
     {
-        sum += pow(-1,i) * (i + 1) * pow(x,i);
+        sum = sum + (pow(-1, i) * (i + 1) * pow(x, i));
     }
     cout << "Значение вычисленного ряда равняется " << sum;
     return 0;
 }
-
