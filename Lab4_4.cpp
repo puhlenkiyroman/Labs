731d8e2cf3fd396d8ea50167aa26bd61485a72f2
Roman Litvinenko <80386499+puhlenkiyroman@users.noreply.github.com>
2021-03-25 16:05:11 +0600

    Update Lab4_4.cpp

--- Lab4_4.cpp
+++ Lab4_4.cpp
@@ -8,40 +8,38 @@ using namespace std;
 
 double find_exp(double x);
 int factorial(const int n);
-void number_or_not(int* number);
-int users_filling();
+double get_elem();
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    printf("Ввведите значение x\n");
-    double x = users_filling();
+    double x = get_elem();
     printf("x = %lf \n", x);
     printf("exp = %lf", find_exp(x));
     return 0;
 }
 
-void number_or_not(int* number)
+double get_elem()
 {
-    int wrong_n = 0;
-    do {
-        const int value_n = scanf_s("%d", number);
-        wrong_n = (value_n != 1);
-        getchar();
-        if (wrong_n)
-        {
-            printf("Вы можете вводить только числа\n");
-            printf("Пожалуйста введите еще раз\n");
-        }
-    } while (wrong_n);
-}
+    double elem;
 
-int users_filling()
-{
-    int element = 0;
-    number_or_not(&element);
-    return element;
+    while (true)
+    {
+        cout << "Enter the element of array: ";
+        cin >> elem;
+
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "It's not correct, please write again" << endl;
+        }
+        else
+        {
+            return elem;
+        }
+    }
 }
 
 double find_exp(double x)
