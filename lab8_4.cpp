1bbc63e4a49233cf63f61b60716b9010a9dde078
Roman Litvinenko <80386499+puhlenkiyroman@users.noreply.github.com>
2021-06-02 14:09:12 +0600

    Update lab8_4.cpp

--- lab8_4.cpp
+++ lab8_4.cpp
@@ -1,89 +1,168 @@
-﻿// lab8_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// lab8_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
+#include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
 class Tree
 {
 private:
-	Tree* one;
-	Tree* two;
-	Tree* three;
-	int n;
+
+    Tree* p_left;
+    Tree* p_right;
+    Tree* p_mid;
+    int n;
+
+    Tree* addElem(Tree* root, int value)
+    {
+        if (root == NULL)
+        {
+            Tree* root = new Tree(value);
+            return root;
+        }
+        else
+        {
+            if (value < root->n)
+            {
+                root->p_left = addElem(root->p_left, value);
+                return root->p_left;
+            }
+            if (value > root->n)
+            {
+                root->p_right = addElem(root->p_right, value);
+                return root->p_right;
+            }
+            else
+            {
+                root->p_mid = addElem(root->p_mid, value);
+                return root->p_mid;
+            }
+        }
+    }
+
+    void printSpace(int c)
+    {
+        for (int i = 0; i < c; i++)
+        {
+            printf(" ");
+        }
+    }
+
+    int indent(Tree* root)
+    {
+        static int c = 0;
+        if (c < 0) { c = 0; }
+
+        if (root->p_left != NULL)
+        {
+            c++;
+            indent(root->p_left);
+        }
+        else if (root->p_mid != NULL)
+        {
+            indent(root->p_mid);
+        }
+        else if (root->p_right != NULL)
+        {
+            c--;
+            indent(root->p_right);
+        }
+
+        return c;
+    }
+
 public:
-	Tree(int n)
-	{
-		this->one = NULL;
-		this->two = NULL;
-		this->three = NULL;
-		this->n = n;
-	}
 
-	Tree* AddElem(Tree* root, int n)
-	{
-		if (root == NULL)
-		{
-			Tree* root = new Tree(n);
-			return root;
-		}
+    Tree(int n) {
+        this->p_right = NULL;
+        this->p_left = NULL;
+        this->p_mid = NULL;
+        this->n = n;
+    }
 
-		else
-		{
+    void addElems(Tree* root, int c)
+    {
+        for (int i = 0; i < c; i++)
+        {
+            root = root->addElem(root, rand() % 10);
+        }
+    }
 
-			if (n < root->n)
-			{
-				root->one = AddElem(root->one, n);
-			}
-
-			if (n == root->n)
-			{
-				root->two = AddElem(root->two, n);
-			}
-
-			if (n > root->n)
-			{
-				root->three = AddElem(root->three, n);
-			}
-
-			return root;
-
-		}
-
-	}
-
-	void PrintTree() 
-	{
-
-		if (this->one != NULL)
-		{
-			this->one->PrintTree();
-		}
-
-		if (this->two != NULL)
-		{
-			this->two->PrintTree();
-		}
-
-		cout << this->n << " ";
-
-		if (this->three != NULL)
-		{
-			this->three->PrintTree();
-		}
-	}
+    void printTree(Tree* root)
+    {
+        static int c = indent(root);
+        if (root->p_left != NULL)
+        {
+            printSpace(c);
+            c--;
+            printf(" %d\n", root->n);
+            c++;
+            printSpace(c);
+            c--;
+            printf("/\n");
+            c--;
+            printTree(root->p_left);
+        }
+        else if (root->p_mid != NULL)
+        {
+            c++;
+            printSpace(c);
+            printf("%d\n", root->n);
+            printSpace(c);
+            printf("|\n");
+            c--;
+            printTree(root->p_mid);
+        }
+        else if (root->p_right != NULL)
+        {
+            c++;
+            printSpace(c);
+            printf("%d \n", root->n);
+            c++;
+            printSpace(c);
+            printf("\\\n");
+            printTree(root->p_right);
+        }
+        else if (root != NULL)
+        {
+            c++;
+            printSpace(c);
+            printf("%d\n", root->n);
+        }
+    }
 
 };
 
-int main()
+int getElem()
 {
-	Tree* root = new Tree(rand() % 10);
-	for (int i = 0; i < 35; i++)
-	{
-		(*root).AddElem(root, rand() % 10);
-	}
-	root->PrintTree();
-	return 0;
+    int elem = 0;
+    while (true)
+    {
+        cout << "How many elements u need in tree?\n";
+        cin >> elem;
+        if (cin.fail())
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "It's not correct, please write again" << endl;
+        }
+        else
+        {
+            return elem - 1;
+            break;
+        }
+    }
+}
+
+int main(void)
+{
+    Tree* root = new Tree(rand() % 10);
+    root->addElems(root, getElem());
+    root->printTree(root);
+
+    return 0;
 }
